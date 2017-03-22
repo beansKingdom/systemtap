2238daa5f6a061e12e756e0baa452a59c8895011
helingyun <764676555@qq.com>
2017-03-22 17:01:15 +0800

    oringi_stap

---
+++ collect_on_off_cpu.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+
+if [[ $1 == "" ]];then
+  echo "Usage:$0 pid stapname"
+  echo "example:$0 3312 cpu_user/cpu_kernel/cpu_all"
+  exit 1
+fi
+
+pid=$1
+stapname=$2
+execdir=`readlink /proc/$pid/exe`
+
+stap -v -D MAXSKIPPED=100000 -D MAXACTION=10000000 -D MAXMAPENTRIES=300000 -g --suppress-time-limits  --skip-badvars --all-modules -x $pid -d $execdir  --ldd  $stapname.stp > out.stap
+
+/home/helingyun/FlameGraph/stackcollapse-stap.pl out.stap > out.folded
+/home/helingyun/FlameGraph/flamegraph.pl out.folded  > out.svg
+
+#/opt/perf_test/FlameGraph/stackcollapse-stap.pl out.stap > out.folded
+#/opt/perf_test/FlameGraph/flamegraph.pl out.folded > out.svg
